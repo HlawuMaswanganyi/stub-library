1eb509045f7cd0db30492968bbd14dfd07c69287
Hlawuleka Maswanganyi <>
2025-02-19 12:05:47 +0200

    feat: add file size checking logic and other tweaks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -15,9 +14,8 @@ export default defineNuxtConfig({
           src: 'https://unpkg.com/@tailwindcss/browser@4',
           type: 'text/javascript',
         },
-      ]
+      ],
     },
   },
-  ssr:true,
-})
-
+  ssr: true,
+});
